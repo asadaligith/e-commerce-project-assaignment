67b6842493b632f7f1b19e0bac51fc641dd51562
Asad <asad2747982@gmail.com>
2026-05-24 15:58:49 +0500

    Backend Created

---
+++ e-commerce-backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors')
+const {config} = require('dotenv')
+const productData = require('./product.json');
+
+config();
+const port = process.env.PORT || 4000;
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+
+let products = productData.products;
+
+app.get('/api', (req, res)=>{
+    res.send("API Running.....")
+
+})
+
+app.get('/api/product', (req,res)=>{
+    res.json({
+           products
+    })
+
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
